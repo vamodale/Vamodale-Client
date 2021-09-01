708eaa1565ce2a76b6c4a3ecd5f47c411d91bbaf
Kastango <metroidling@gmail.com>
2021-08-31 23:17:39 -0300

    inicio oauth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -18,6 +18,14 @@ interface IAuthContextData {
   signInWithGoogle(): Promise<void>
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
+
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps ){
@@ -37,8 +45,16 @@ function AuthProvider({ children }: AuthProviderProps ){
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response)
+      const {type, params} = await AuthSession
+      .startAsync({ authUrl }) as AuthorizationResponse
+
+      if (type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        console.log(userInfo);
+
+      }
 
     } catch (error) {
       throw new Error(error);
